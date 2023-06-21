43c104ff485291021c08b427b3352261088fa86b
Domse <102017735+d0mse@users.noreply.github.com>
2023-06-21 20:23:31 +0200

    changed ('node:path') to ('path')

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('node:path');
+const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
