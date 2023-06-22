1be6484633ba9ff3506c3af3bc815c0251c74479
d0mse <102017735+d0mse@users.noreply.github.com>
2023-06-22 07:47:00 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
